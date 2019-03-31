77ee65a5ea495c9632388dded6cb638f0573a29f
Joe <joe.l.filbrun@gmail.com>
2019-03-31 17:49:42 -0400

    initial commit

---
+++ init.lua
@@ -0,0 +1,16 @@
+local units = {
+    {category="Filtering"},
+
+    {title="Scorpio Vocoder", moduleName="Scorpio", keywords="filter, modulate"},
+
+    {category="Oscillators"},
+    {title="Evil Twin", moduleName="EvilTwin", keywords="oscillator"}
+}
+  return {
+    title = "Unions",
+    name = "Joe Filbrun, Steve Richardson",
+    contact = "via O|D Forums @Joe, @kel",
+    keyword = "Unions",
+    units = units
+  }
+  
